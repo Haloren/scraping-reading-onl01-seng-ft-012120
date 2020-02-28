492753c34817bfd6c25d6f98f0117284d91abfbd
Kyle McKenna <mckennakyler@gmail.com>
2020-02-28 19:33:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,5 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
